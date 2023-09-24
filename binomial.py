48c49918246ad64e303747761d4c42e5614970f5
wlauer3 <wlauer3@gatech.edu>
2023-09-23 20:07:15 -0400

    changed way scatterplot works. not entirely correct

--- binomial.py
+++ binomial.py
@@ -13,32 +13,34 @@ def go(probability):
 # Initialize scatterplot_data at the beginning
 scatterplot_data = []
 
-def run(population, links, probability):
-    array = []
+def run(population, max_links, probability):
     pop2 = population
-    while population > 0:
-        target_good = links
-        total_trials = 0
-        good_trials = 0
+    for links in range(1, max_links + 1):
+        avg_resends = 0  # Initialize the average resends for each set of trials
+        for _ in range(population):
+            total_trials = 0
+            good_trials = 0
 
-        while good_trials < target_good:
-            result = go(probability)
-            total_trials += 1
+            while good_trials < links:
+                result = go(probability)
+                total_trials += 1
 
-            if result == "Success":
-                good_trials += 1
-        array.append(total_trials)
-        population -= 1
-
-    avg = sum(array) / len(array)
-    result_label.config(text=f"With a sample size of {pop2} packets, on average, it took {avg:.2f} resends to get 1 packet across {links} nodes.")
+                if result == "Success":
+                    good_trials += 1
+                else:
+                    good_trials = 0
+            
+            avg_resends += total_trials
+        
+        avg_resends /= population  # Calculate the average resends for this set of trials
+        
+        result_label.config(text=f"With a sample size of {pop2} packets, on average, it took {avg_resends:.2f} resends to get 1 packet across {links} nodes.")
+        
+        # Add the data point to the scatterplot
+        scatterplot_data.append((links, avg_resends))
     
-    # Add the data point to the scatterplot
-    scatterplot_data.append((probability, avg))
+    # After the loop, update the scatterplot
     update_scatterplot()
-    
-    # Update the convergence result text
-    convergence_result_label.config(text=f"The results converge to {1/probability * links:.2f} resends.")
 
 def reset_data():
     # Clear the scatterplot_data list to remove all drawn dots
@@ -47,7 +49,6 @@ def reset_data():
     # Clear the scatterplot and update result labels
     update_scatterplot()
     result_label.config(text="")
-    convergence_result_label.config(text="")
 
 def update_scatterplot():
     # Clear the previous plot
@@ -60,17 +61,36 @@ def update_scatterplot():
         # Create the scatterplot
         scatterplot_ax.scatter(x, y, marker='o', color='blue')
     
-    scatterplot_ax.set_xlabel('Probability')
+    scatterplot_ax.set_xlabel('Nodes')
     scatterplot_ax.set_ylabel('Average Resends')
     
     # Draw the updated scatterplot on the canvas
     scatterplot_canvas.draw()
 
+def on_scatter_click(event):
+    # Convert the click coordinates to data coordinates
+    x_data, y_data = scatterplot_ax.transData.inverted().transform([event.x, event.y])
+    
+    # Find the closest point in the scatterplot to the clicked point
+    if scatterplot_data:
+        min_distance = float('inf')
+        closest_point = None
+        for x, y in scatterplot_data:
+            distance = ((x_data - x)**2 + (y_data - y)**2)**0.5
+            if distance < min_distance:
+                min_distance = distance
+                closest_point = (x, y)
+        
+        if closest_point:
+            x, y = closest_point
+            clicked_point_info = f"Node: {int(x)}, Avg Resends: {y:.2f}"
+            result_label.config(text=clicked_point_info)
+
 def run_button_click():
     pop = int(population_entry.get())
-    lnk = int(links_entry.get())
+    max_links = int(links_entry.get())
     prob = float(probability_entry.get())
-    run(pop, lnk, prob)
+    run(pop, max_links, prob)
 
 # Create the main window
 root = tk.Tk()
@@ -87,7 +107,7 @@ population_label.pack()
 population_entry = tk.Entry(root, justify="center")
 population_entry.pack()
 
-links_label = tk.Label(root, text="Number of Nodes:", font=("Helvetica", 12))
+links_label = tk.Label(root, text="Maximum Number of Nodes:", font=("Helvetica", 12))
 links_label.pack()
 
 links_entry = tk.Entry(root, justify="center")
@@ -112,7 +132,7 @@ reset_button.pack()
 
 # Scatterplot
 scatterplot_fig, scatterplot_ax = plt.subplots()
-scatterplot_ax.set_xlabel('Probability')
+scatterplot_ax.set_xlabel('Nodes')
 scatterplot_ax.set_ylabel('Average Resends')
 
 scatterplot_frame = ttk.Frame(root)
@@ -122,8 +142,8 @@ scatterplot_canvas = FigureCanvasTkAgg(scatterplot_fig, master=scatterplot_frame
 scatterplot_canvas_widget = scatterplot_canvas.get_tk_widget()
 scatterplot_canvas_widget.pack(fill="both", expand=True)
 
-# Text Label for Convergence Result
-convergence_result_label = tk.Label(root, text="", font=("Helvetica", 14, "bold"))
-convergence_result_label.pack(pady=10)
+# Connect the click event handler to the scatterplot
+scatterplot_canvas_widget.bind('<ButtonPress-1>', on_scatter_click)
 
+# Start the Tkinter main event loop
 root.mainloop()
